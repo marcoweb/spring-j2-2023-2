54b8033135eb1e512ff0d510decde9fd949ea4e7
marcoweb <marcocarvalho.web@gmail.com>
2023-10-24 17:04:53 +0000

    List Aluno

--- app/src/main/java/application/controller/AlunoController.java
+++ app/src/main/java/application/controller/AlunoController.java
@@ -2,6 +2,7 @@ package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import application.model.AlunoRepository;
@@ -13,7 +14,12 @@ public class AlunoController {
     private AlunoRepository alunoRepo;
 
     @RequestMapping("/list")
-    public String list() {
+    public String list(Model ui) {
+        ui.addAttribute("alunos", alunoRepo.findAll());
         return "/aluno/list";
+    }
+
+    public String insert() {
+        return "/aluno/insert";
     }
 }
